4cc627b5fd5bff309438c93b0dbc41d586558f0f
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2021-08-26 00:32:47 +1200

    Added projects section

---
+++ components/ProjectCard.js
@@ -0,0 +1,31 @@
+import Image from "next/image"
+import styles from "../styles/ProjectCard.module.scss"
+
+
+function ProjectCard(props) {
+    return(
+        <a href={props.url} target="_blank" >
+            <div className={styles.card}>
+                {/* <div className={styles.cardImage}> */}
+                    {props.image?
+                        <Image
+                            className={styles.cardImage}
+                            src={props.image}
+                            alt={props.title}
+                        />:
+                        <h3></h3>
+                    }
+                {/* </div> */}
+                <div className={styles.cardText}>
+                    <h3>{props.title}</h3>
+                    <p>{props.description}</p>
+                </div>
+
+
+            </div>
+        </a>
+    )
+}
+
+
+export default ProjectCard
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,14 @@
 import styles from '../styles/Home.module.scss'
+import ProjectCard from '../components/ProjectCard'
+
+
+// images
+import calculatorImage from "../images/projects/calculator.png"
+import pixelDraw from "../images/projects/pixel_draw.png"
+import rockPaperScissors from "../images/projects/rock_paper_scissors.png"
+import ticTacToe from "../images/projects/tic_tac_toe.png"
+import libraryImage from "../images/projects/library.png"
+import animeSearch from "../images/projects/animeSearch.jpg"
 
 export default function Home() {
   return (
@@ -6,12 +16,58 @@ export default function Home() {
       <div className={styles.banner}>
         <div className={styles.about}>
           <div className={styles.aboutText}>
-            <h2>About me</h2>
-            <p>Hi, I'm Jacky. A student studying Computer Science, and Information Technology Management.</p>
+            <h2>About</h2>
+            <p>Hi, I'm Jacky. A student studying Computer Science and Information Technology Management.</p>
           </div>
         </div>
       </div>
-      <p>Hello World</p>
+      <div className={styles.project}>
+          <h2>
+            Programming Projects
+          </h2>
+          {/* This is where my projects go */}
+          <div className={styles.container}> 
+            <ProjectCard
+              image={animeSearch}
+              title="Anime Character Search"
+              url="https://github.com/p4ckysm4cky/search_anime_character"
+              description={<p>A <b>React App</b> that uses <b>ApolloClient</b> to search for anime characters through <b>GraphQL</b> using the Anilist API.</p>}
+           
+            />
+            <ProjectCard
+              image={libraryImage}
+              title="Library Reader Tracker"
+              url="https://github.com/p4ckysm4cky/library_app"
+              description={<p>A <b>Object-Orientated-Programming</b> based webapp that keeps track of books that you have read. it uses <b>Javascript localstorage</b> to store the information.</p>}
+            />
+
+            <ProjectCard
+              image={calculatorImage}
+              title="Calculator"
+              url="https://github.com/p4ckysm4cky/web_calculator"
+              description={<p>A GUI calculator based on the Dracula colour scheme, which I made to practise <b>CSS Grid</b>.</p>}
+            />
+             <ProjectCard
+              image={ticTacToe}
+              title="Tic Tac Toe"
+              url="https://github.com/p4ckysm4cky/Tic-Tac-Toe"
+              description={<p>A project I made to learn about <b>Javascript Classes</b> and <b>Object-Orientated-Programming</b>.</p>}
+            />
+            <ProjectCard
+              image={pixelDraw}
+              title="Pixel Draw"
+              url="https://github.com/p4ckysm4cky/pixel_doodle"
+              description={<p>A webapp that allows you to draw pixel art with your mouse. This project helped me understand <b>Javascript DOM Events</b>.</p>}
+            />
+            <ProjectCard
+              image={rockPaperScissors}
+              title="Rock Paper Scissors"
+              url="https://github.com/p4ckysm4cky/rock_paper_scissors"
+              description={<p>I made this webapp, so I could practise the basics of <b>CSS</b> and <b>Flexbox</b>.</p>}            
+            />
+
+          </div>
+      </div>
     </div>
 
   )
